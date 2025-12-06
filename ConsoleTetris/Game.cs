27a9a108549db157d4644cb39153538cb616db51
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-12-06 22:24:43 +0100

    Add game class with todo comments

---
+++ ConsoleTetris/Game.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleTetris;
+
+internal class Game
+{
+    public void Start()
+    {
+        Initialize();
+        Run();
+    }
+
+    public void Stop()
+    {
+        // TODO: Stop the game loop.
+    }
+
+
+
+    private void Run()
+    {
+        while (true) // TODO: Implement exit condition.
+        {
+            MainLoop();
+        }
+    }
+
+    private void Initialize()
+    {
+        // TODO: Initialize game.
+        // - Set up game board.
+        // - Initialize score.
+        // - Set up input handling.
+        // - Set up rendering.
+    }
+
+    private void MainLoop()
+    {
+        // TODO:
+        // - Handle input.
+        // - Update game state.
+        // - Render output.
+    }
+}
