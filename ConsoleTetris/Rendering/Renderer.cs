a6610304d764a66b92ca75a71dabbfd8fb9a33b5
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-14 21:36:53 +0100

    Add renderer

---
+++ ConsoleTetris/Rendering/Renderer.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleTetris.Rendering;
+
+internal class Renderer
+{
+    public void DrawFrame(int x, int y, int width, int height, bool doubleWall = false)
+    {
+        string parts = doubleWall ? "═║╔╗╚╝" : "─│┌┐└┘";
+        string horizontalLine = new string(parts[0], width - 2); // Reusable horizontal line without corners.
+
+        // Top part
+        Console.SetCursorPosition(x, y);
+        Console.Write(parts[2] + horizontalLine + parts[3]);
+
+        // Middle part (sides).
+        for (int i = 1; i < height - 1; i++)
+        {
+            Console.SetCursorPosition(x, y + i);
+            Console.Write(parts[1]);
+            Console.SetCursorPosition(x + width - 1, y + i);
+            Console.Write(parts[1]);
+        }
+
+        // Bottom part.
+        Console.SetCursorPosition(x, y + height - 1);
+        Console.Write(parts[4] + horizontalLine + parts[5]);
+    }
+}
