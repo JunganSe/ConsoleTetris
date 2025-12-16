2d7c1b3e280e1b49e81f7dac77c15449c2fb47f3
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-16 21:50:03 +0100

    Put window size in constants

--- ConsoleTetris/Global/GlobalConstants.cs
+++ ConsoleTetris/Global/GlobalConstants.cs
@@ -2,6 +2,12 @@
 
 internal class GlobalConstants
 {
+    public class WindowSize
+    {
+        public const int Width = PlayArea.X * 2 + PlayArea.Width * 2 + 3;
+        public const int Height = PlayArea.Y + PlayArea.Height + 2;
+    }
+
     public class PlayArea
     {
         public const int X = 2;
--- ConsoleTetris/Rendering/ConsoleManager.cs
+++ ConsoleTetris/Rendering/ConsoleManager.cs
@@ -6,9 +6,6 @@ internal static class ConsoleManager
     {
         Console.CursorVisible = false;
         Console.Clear();
-
-        int width = GlobalConstants.PlayArea.X * 2 + GlobalConstants.PlayArea.Width * 2 + 3;
-        int height = GlobalConstants.PlayArea.Y + GlobalConstants.PlayArea.Height + 2;
-        Console.SetWindowSize(width, height);
+        Console.SetWindowSize(GlobalConstants.WindowSize.Width, GlobalConstants.WindowSize.Height);
     }
 }
