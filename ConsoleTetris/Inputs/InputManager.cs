4fe43f102e9dda4d0dd8d003279008f6b209711d
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-17 22:48:36 +0100

    Add InputManager

---
+++ ConsoleTetris/Inputs/InputManager.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleTetris.Inputs;
+
+internal class InputManager
+{
+    private HashSet<Key> _currentHeldKeys = [];
+    private HashSet<Key> _previousHeldKeys = [];
+
+    /// <summary>
+    /// Updates the state of held keys. Should be called once per frame.
+    /// </summary>
+    public void Update()
+    {
+        _previousHeldKeys = _currentHeldKeys;
+        _currentHeldKeys = InputReader.GetHeldKeys<Key>();
+    }
+
+    public bool IsKeyHeld(Key key) =>
+        _currentHeldKeys.Contains(key);
+
+    public bool IsKeyPressed(Key key) =>
+        _currentHeldKeys.Contains(key) && !_previousHeldKeys.Contains(key);
+
+    public bool IsKeyReleased(Key key) =>
+        !_currentHeldKeys.Contains(key) && _previousHeldKeys.Contains(key);
+}
