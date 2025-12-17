d785ef1bc50aecfe1c82b8ef9b0b998e5a3e7774
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-17 22:01:05 +0100

    Move virtual keys to enum

--- ConsoleTetris/Inputs/Enums.cs
+++ ConsoleTetris/Inputs/Enums.cs
@@ -12,3 +12,18 @@ public enum Input
     Pause,
     Quit,
 }
+
+/// <summary> Virtual key codes </summary>
+public enum Key
+{
+    Up = 0x26,
+    Down = 0x28,
+    Left = 0x25,
+    Right = 0x27,
+    A = 0x41,
+    S = 0x53,
+    D = 0x44,
+    Enter = 0x0D,
+    Space = 0x20,
+    Escape = 0x1B,
+}
