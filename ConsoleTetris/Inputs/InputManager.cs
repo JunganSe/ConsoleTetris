d69084c6f8f7acd23f002f297d1ad03ad2cc9693
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-20 22:29:11 +0100

    Add InputState

--- ConsoleTetris/Inputs/InputManager.cs
+++ ConsoleTetris/Inputs/InputManager.cs
@@ -5,23 +5,28 @@ internal class InputManager
     private HashSet<Input> _currentHeldInputs = [];
     private HashSet<Input> _previousHeldInputs = [];
 
-    /// <summary>
-    /// Updates the state of held inputs. Should be called once per frame.
-    /// </summary>
+    /// <summary> Updates the state of held inputs. Should be called once per frame. </summary>
     public void Update()
     {
-        _previousHeldInputs = _currentHeldInputs;
+        _previousHeldInputs = [.. _currentHeldInputs];
         _currentHeldInputs = InputReader.GetHeldKeys<Key>()
                                         .Select(KeyMapper.Map)
                                         .ToHashSet();
     }
 
-    public bool IsHeld(Input input) =>
-        _currentHeldInputs.Contains(input);
-
-    public bool IsPressed(Input input) =>
-        _currentHeldInputs.Contains(input) && !_previousHeldInputs.Contains(input);
-
-    public bool IsReleased(Input input) =>
-        !_currentHeldInputs.Contains(input) && _previousHeldInputs.Contains(input);
+    /// <summary> Gets a snapshot of the current input state. </summary>
+    /// <remarks> This is typically called after updating the input state with <see cref="Update"/>. </remarks>
+    public InputState GetInputState()
+    {
+        var pressedInputs = _currentHeldInputs
+            .Where(input => !_previousHeldInputs.Contains(input))
+            .ToHashSet();
+        var releasedInputs = _previousHeldInputs
+            .Where(input => !_currentHeldInputs.Contains(input))
+            .ToHashSet();
+        return new InputState(
+            Held: new HashSet<Input>(_currentHeldInputs),
+            Pressed: pressedInputs,
+            Released: releasedInputs);
+    }
 }
---
+++ ConsoleTetris/Inputs/InputState.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleTetris.Inputs;
+
+internal record InputState(
+    IReadOnlySet<Input> Held,
+    IReadOnlySet<Input> Pressed,
+    IReadOnlySet<Input> Released)
+{
+    public bool IsHeld(Input input) => Held.Contains(input);
+    public bool IsPressed(Input input) => Pressed.Contains(input);
+    public bool IsReleased(Input input) => Released.Contains(input);
+}
