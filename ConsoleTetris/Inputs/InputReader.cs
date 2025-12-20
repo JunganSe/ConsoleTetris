dd441b78474cb8f9b45b7ac43c38e61ff12cfe1e
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-20 21:41:20 +0100

    Refactor InputReader

--- ConsoleTetris/Inputs/InputReader.cs
+++ ConsoleTetris/Inputs/InputReader.cs
@@ -4,25 +4,23 @@ namespace ConsoleTetris.Inputs;
 
 internal partial class InputReader
 {
-    [LibraryImport("user32.dll")]
-    private static partial short GetAsyncKeyState(int vKey);
+    // Gets a set of all keys in the provided integer enum that are currently being held down.
+    public static HashSet<TKey> GetHeldKeys<TKey>() where TKey : struct, Enum
+    {
+        return Enum.GetValues<TKey>()
+            .Where(key => IsKeyHeld(Convert.ToInt32(key)))
+            .ToHashSet();
+    }
+
+
 
     // Checks if a specific key is currently being held down.
-    public static bool IsKeyHeld(int vKey)
+    private static bool IsKeyHeld(int vKey)
     {
         short state = GetAsyncKeyState(vKey);
         return (state & 0x8000) != 0;
     }
 
-    // Gets a set of all keys in the provided enum that are currently being held down.
-    public static HashSet<TKey> GetHeldKeys<TKey>() where TKey : struct, Enum
-    {
-        var heldKeys = new HashSet<TKey>();
-        foreach (TKey key in Enum.GetValues<TKey>())
-        {
-            if (IsKeyHeld(Convert.ToInt32(key)))
-                heldKeys.Add(key);
-        }
-        return heldKeys;
-    }
+    [LibraryImport("user32.dll")]
+    private static partial short GetAsyncKeyState(int vKey);
 }
