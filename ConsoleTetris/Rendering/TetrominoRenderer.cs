3e4d1f8a3d226b31b00efe273e26dca88aa25c2e
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-21 16:36:43 +0100

    Add tetromino renderer (unimplemented)

---
+++ ConsoleTetris/Rendering/TetrominoRenderer.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleTetris.Game;
+
+namespace ConsoleTetris.Rendering;
+
+internal class TetrominoRenderer
+{
+    public void DrawTetromino(Tetromino tetromino)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MoveTetromino(Tetromino tetromino, int oldX, int oldY)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RotateTetromino(Tetromino tetromino, int oldDirection)
+    {
+        throw new NotImplementedException();
+    }
+}
