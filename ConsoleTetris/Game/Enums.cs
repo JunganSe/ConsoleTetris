8532e9e885d1f34918a577d356c041392f42b93f
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-21 16:35:18 +0100

    Add tetromino model and enum

---
+++ ConsoleTetris/Game/Enums.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleTetris.Game;
+
+internal enum TetrominoType
+{
+    I,
+    O,
+    T,
+    S,
+    Z,
+    J,
+    L,
+}
---
+++ ConsoleTetris/Game/Tetromino.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleTetris.Game;
+
+internal class Tetromino
+{
+    public TetrominoType Type { get; init; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Direction { get; set; }
+}
