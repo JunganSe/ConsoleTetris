e5cc2d19685d5a1c9b4917afc9b4f76576f47a25
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-22 21:50:08 +0100

    Add TetrominoRenderer todo comments

--- ConsoleTetris/Rendering/TetrominoRenderer.cs
+++ ConsoleTetris/Rendering/TetrominoRenderer.cs
@@ -6,16 +6,21 @@ internal class TetrominoRenderer
 {
     public void DrawTetromino(Tetromino tetromino)
     {
+        // TODO: Draw all pieces of the tetromino.
         throw new NotImplementedException();
     }
 
     public void MoveTetromino(Tetromino tetromino, int oldX, int oldY)
     {
+        // TODO: Erase only the pieces of the tetromino that have moved,
+        //       and draw only the new pieces.
         throw new NotImplementedException();
     }
 
     public void RotateTetromino(Tetromino tetromino, int oldDirection)
     {
+        // TODO: Erase only the pieces of the tetromino that have moved,
+        //       and draw only the new pieces.
         throw new NotImplementedException();
     }
 }
