e61423fbc3e87103bf516e38238a7ebf7e882687
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-22 21:40:36 +0100

    Add Playfield

---
+++ ConsoleTetris/Game/Playfield.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleTetris.Game;
+
+internal class Playfield
+{
+    /// <summary> The individual pieces of the tetrominos in the playfield. </summary>
+    /// <remarks> Defined as [x, y] where bottom left is (0,0). </remarks>
+    public TetrominoType?[,] Pieces { get; init; }
+
+    public Playfield()
+    {
+        Pieces = new TetrominoType?[10, 20];
+    }
+}
