1c7ec6a30730775c5d2f3fb8636c55f0e1fd1690
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-22 21:55:09 +0100

    Add ColorMapper

---
+++ ConsoleTetris/Rendering/ColorMapper.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleTetris.Game;
+
+namespace ConsoleTetris.Rendering;
+
+internal class ColorMapper
+{
+    public static ConsoleColor GetTetrominoColor(TetrominoType tetrominoType) => tetrominoType switch
+    {
+        TetrominoType.I => ConsoleColor.Cyan,
+        TetrominoType.O => ConsoleColor.Yellow,
+        TetrominoType.T => ConsoleColor.Magenta,
+        TetrominoType.S => ConsoleColor.Green,
+        TetrominoType.Z => ConsoleColor.Red,
+        TetrominoType.J => ConsoleColor.Blue,
+        TetrominoType.L => ConsoleColor.DarkYellow,
+        _ => ConsoleColor.White,
+    };
+}
