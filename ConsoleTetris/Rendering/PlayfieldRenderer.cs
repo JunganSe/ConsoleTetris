f03244fc170eeeed925dbe0853647dbfbe597845
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2025-12-23 21:51:08 +0100

    Refactor PlayfieldRenderer

--- ConsoleTetris/Rendering/PlayfieldRenderer.cs
+++ ConsoleTetris/Rendering/PlayfieldRenderer.cs
@@ -6,13 +6,18 @@ internal class PlayfieldRenderer
 {
     public void DrawPlayfield(Playfield playfield)
     {
-        for (int x = 0; x < playfield.Pieces.GetLength(0); x++)
+        int playfieldWidth = playfield.Pieces.GetLength(0);
+        int playfieldHeight = playfield.Pieces.GetLength(1);
+        int cursorOffsetX = GlobalConstants.PlayArea.X * 2;
+        int cursorOffsetY = GlobalConstants.PlayArea.Y + GlobalConstants.PlayArea.Height - 1;
+
+        for (int x = 0; x < playfieldWidth; x++)
         {
-            for (int y = 0; y < playfield.Pieces.GetLength(1); y++)
+            for (int y = 0; y < playfieldHeight; y++)
             {
-                int cx = GlobalConstants.PlayArea.X * 2 + x * 2;
-                int cy = GlobalConstants.PlayArea.Y + (GlobalConstants.PlayArea.Height - 1 - y);
-                Console.SetCursorPosition(cx, cy);
+                int cursorX = cursorOffsetX + x * 2;
+                int cursorY = cursorOffsetY - y;
+                Console.SetCursorPosition(cursorX, cursorY);
 
                 var piece = playfield.Pieces[x, y];
                 if (piece.HasValue)
