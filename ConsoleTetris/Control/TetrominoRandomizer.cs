12b9856b1e1a3b14e2404d15ae23261d6bd7fabe
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2026-01-04 20:45:53 +0100

    Add TetrominoRandomizer

---
+++ ConsoleTetris/Control/TetrominoRandomizer.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleTetris.GameComponents;
+
+namespace ConsoleTetris.Control;
+
+internal class TetrominoRandomizer
+{
+    private readonly Random _random = new();
+    private readonly Stack<TetrominoShape> _bag = new();
+    private readonly TetrominoShape[] _possibleShapes = Enum.GetValues<TetrominoShape>();
+
+    public TetrominoShape GetNext()
+    {
+        if (_bag.Count == 0)
+            FillBag();
+
+        return _bag.Pop();
+    }
+
+    public TetrominoShape PeekNext()
+    {
+        if (_bag.Count == 0)
+            FillBag();
+
+        return _bag.Peek();
+    }
+
+    private void FillBag()
+    {
+        var shapes = _possibleShapes.OrderBy(_ => _random.Next()).ToArray();
+        foreach (var shape in shapes)
+            _bag.Push(shape);
+    }
+}
