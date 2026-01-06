430b238b6ba2dd18ccf3585c6908a37c946493c5
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2026-01-06 19:15:58 +0100

    Combine primary and secondary coords in tetromino

--- ConsoleTetris/GameComponents/Tetromino.cs
+++ ConsoleTetris/GameComponents/Tetromino.cs
@@ -21,62 +21,57 @@ internal class Tetromino
 
 
 
-    /// <summary> Gets an array of coordinates for the secondary pieces. </summary>
     private Coord[] GetPiecesCoords()
     {
-        var secondaryPiecesCoords = GetRelativeSecondaryPiecesCoords()
-            .Select(coord => new Coord(X + coord.X, Y + coord.Y));
-        return [new Coord(X, Y), .. secondaryPiecesCoords];
+        return GetRelativePiecesCoords()
+            .Select(coord => new Coord(X + coord.X, Y + coord.Y))
+            .ToArray();
     }
 
-    /// <summary>
-    /// Gets an array of coordinates for the secondary pieces,
-    /// relative to the primary piece at the tetromino's coordinates.
-    /// </summary>
-    private Coord[] GetRelativeSecondaryPiecesCoords() => Shape switch
+    private Coord[] GetRelativePiecesCoords() => Shape switch
     {
         TetrominoShape.I => Direction switch
         {
-            Direction.A or Direction.C => [new(-1, 0), new(1, 0), new(2, 0)],
-            Direction.D or Direction.B => [new(0, -1), new(0, 1), new(0, 2)],
+            Direction.A or Direction.C => [new(-1, 0), new(0, 0), new(1, 0), new(2, 0)],
+            Direction.D or Direction.B => [new(0, -1), new(0, 0), new(0, 1), new(0, 2)],
         },
 
-        TetrominoShape.O => [new(0, -1), new(1, -1), new(1, 0)],
+        TetrominoShape.O => [new(0, -1), new(0, 0), new(1, -1), new(1, 0)],
 
         TetrominoShape.T => Direction switch
         {
-            Direction.A => [new(0, -1), new(-1, 0), new(1, 0)],
-            Direction.B => [new(0, -1), new(-1, 0), new(0, 1)],
-            Direction.C => [new(-1, 0), new(1, 0), new(0, 1)],
-            Direction.D => [new(0, -1), new(1, 0), new(0, 1)],
+            Direction.A => [new(0, -1), new(-1, 0), new(0, 0), new(1, 0)],
+            Direction.B => [new(0, -1), new(-1, 0), new(0, 0), new(0, 1)],
+            Direction.C => [new(-1, 0), new(0, 0), new(1, 0), new(0, 1)],
+            Direction.D => [new(0, -1), new(0, 0), new(1, 0), new(0, 1)],
         },
 
         TetrominoShape.S => Direction switch
         {
-            Direction.A or Direction.C => [new(-1, -1), new(0, -1), new(1, 0)],
-            Direction.B or Direction.D => [new(1, -1), new(1, 0), new(0, 1)],
+            Direction.A or Direction.C => [new(-1, -1), new(0, -1), new(0, 0), new(1, 0)],
+            Direction.B or Direction.D => [new(1, -1), new(0, 0), new(1, 0), new(0, 1)],
         },
 
         TetrominoShape.Z => Direction switch
         {
-            Direction.A or Direction.C => [new(0, -1), new(1, -1), new(-1, 0)],
-            Direction.B or Direction.D => [new(0, -1), new(1, 0), new(1, 1)],
+            Direction.A or Direction.C => [new(0, -1), new(1, -1), new(-1, 0), new(0, 0)],
+            Direction.B or Direction.D => [new(0, -1), new(0, 0), new(1, 0), new(1, 1)],
         },
 
         TetrominoShape.J => Direction switch
         {
-            Direction.A => [new(1, -1), new(-1, 0), new(1, 0)],
-            Direction.B => [new(-1, -1), new(0, -1), new(0, 1)],
-            Direction.C => [new(-1, 0), new(1, 0), new(-1, 1)],
-            Direction.D => [new(0, -1), new(0, 1), new(1, 1)],
+            Direction.A => [new(1, -1), new(-1, 0), new(0, 0), new(1, 0)],
+            Direction.B => [new(-1, -1), new(0, -1), new(0, 0), new(0, 1)],
+            Direction.C => [new(-1, 0), new(0, 0), new(1, 0), new(-1, 1)],
+            Direction.D => [new(0, -1), new(0, 0), new(0, 1), new(1, 1)],
         },
 
         TetrominoShape.L => Direction switch
         {
-            Direction.A => [new(-1, -1), new(-1, 0), new(1, 0)],
-            Direction.B => [new(0, -1), new(-1, 1), new(0, 1)],
-            Direction.C => [new(-1, 0), new(1, 0), new(1, 1)],
-            Direction.D => [new(0, -1), new(1, -1), new(0, 1)],
+            Direction.A => [new(-1, -1), new(-1, 0),new(0, 0),  new(1, 0)],
+            Direction.B => [new(0, -1), new(0, 0), new(-1, 1), new(0, 1)],
+            Direction.C => [new(-1, 0), new(0, 0), new(1, 0), new(1, 1)],
+            Direction.D => [new(0, -1), new(1, -1), new(0, 0), new(0, 1)],
         }
     };
 }
