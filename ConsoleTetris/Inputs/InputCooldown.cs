be69ee40cc7bbb654a38313641042ff9be6c0f39
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2026-01-12 21:32:29 +0100

    Add InputCooldown

---
+++ ConsoleTetris/Inputs/InputCooldown.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleTetris.Inputs;
+
+public class InputCooldown
+{
+    private readonly Dictionary<Input, int> _cooldowns = [];
+    private readonly Dictionary<Input, int> _framesSinceInput = [];
+
+    /// <remarks> Call once per frame. </remarks>
+    public void Update()
+    {
+        foreach (var input in _cooldowns.Keys.ToList())
+        {
+            if (_framesSinceInput[input] < _cooldowns[input])
+                _framesSinceInput[input]++;
+        }
+    }
+
+    public void SetCooldown(Input input, int frames)
+    {
+        _cooldowns[input] = frames;
+        if (!_framesSinceInput.ContainsKey(input))
+            _framesSinceInput[input] = frames;
+    }
+
+    public bool IsReady(Input input)
+    {
+        return !_cooldowns.TryGetValue(input, out int value)
+            || _framesSinceInput[input] >= value;
+    }
+
+    public void Reset(Input input)
+    {
+        if (_framesSinceInput.ContainsKey(input))
+            _framesSinceInput[input] = 0;
+    }
+}
