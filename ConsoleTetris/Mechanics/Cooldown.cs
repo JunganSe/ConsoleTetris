b10888dd817c77ff11735805480d2ee300249f6b
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2026-01-31 20:29:42 +0100

    Count down in Cooldown instead of up

--- ConsoleTetris/Mechanics/Cooldown.cs
+++ ConsoleTetris/Mechanics/Cooldown.cs
@@ -1,18 +1,17 @@
 ﻿namespace ConsoleTetris.Mechanics;
 
-// TODO: Count down instead of up.
 internal class Cooldown
 {
     private int _cooldown;
-    private int _elapsedFrames;
+    private int _remainingFrames;
 
     public bool IsActive { get; set; } = true;
 
     /// <remarks> Call once per frame. </remarks>
     public void Update()
     {
-        if (IsActive && _elapsedFrames < _cooldown)
-            _elapsedFrames++;
+        if (IsActive && _remainingFrames > 0)
+            _remainingFrames--;
     }
 
     public void SetCooldown(int frames)
@@ -20,13 +19,18 @@ internal class Cooldown
         _cooldown = frames;
     }
 
+    public void Ready()
+    {
+        _remainingFrames = 0;
+    }
+
     public bool IsReady()
     {
-        return (IsActive && _elapsedFrames >= _cooldown);
+        return (IsActive && _remainingFrames <= 0);
     }
 
     public void Reset()
     {
-        _elapsedFrames = 0;
+        _remainingFrames = _cooldown;
     }
 }
